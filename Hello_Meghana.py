3ca1c3af702f25460a25e49f6ae11d5921cf0bc8
MeghanaGantla <meghana_gantla@gwu.edu>
2022-01-19 03:08:54 +0530

    Code V3

--- Hello_Meghana.py
+++ Hello_Meghana.py
@@ -8,49 +8,23 @@ print(9%2, 2%9) # print remainders from divisions
 
 #%%
 astring = "Good Morning"
-anum = 3.14159265358979323846
+numb = 3.1415926535
 cnt = 1
 # Many different ways to print out the same line
 print("%d. I wish you a very %s" % (cnt,astring) )
 cnt+=1
 print(cnt,". I wish you a very " + astring )
 cnt+=1
-print(cnt, ". I wish you a very",astring )
+print(cnt, ". I wish you a very ",astring )
 cnt+=1
-print("%d. I wish you a very %s, my sweetie %.3f" % (cnt,astring,anum) )
+print("%d. I wish you a very %s, we made apple %.3f for breakfast" % (cnt,astring,numb) )
 cnt+=1
-print("%d. I wish you a very %s, my sweetie digit %d" % (cnt,astring,anum) )
+print("%d. I wish you a very %s, we have %d apples in the basket" % (cnt,astring,numb) )
 cnt+=1
-print("%d. I wish you a very %s, my sweetie long %f" % (cnt,astring,anum) )
+print("%d. I wish you a very %s, we made a long apple %f for you " % (cnt,astring,numb) )
 cnt+=1
 # For python 3.6+, we can use the f-string
-print(f"{cnt}. I wish you a very {astring}, my sweetie long {anum.__round__(3)}")
+print(f"{cnt}. I wish you a very {astring}, we made apple {numb.__round__(3)} for breakfast")
 cnt+=1
+
 # see https://python-reference.readthedocs.io/en/latest/docs/str/formatting.html
-# s-string, d-digit (int), f-float
-# 
-# side note:  
-# for more info on the new python f-string since python 3.6, see
-# for example: https://cito.github.io/blog/f-strings/
-#
-# also, see https://docs.python.org/3/reference/lexical_analysis.html#literals
-# for info on f-string, r-string, b-string, etc.
-
-
-#%%[markdown]
-#
-# # Python Class 01
-# 
-# This is our first class of the semester.
-# Hello to everyone.   
-# Two spaces in the previous line doesn't make a new line in this environment. 
-#
-# You will need a blank line to get a new paragraph.
-
-# The above is not considered a blank line without the # sign.
-#
-# This can get you a [link](http://www.gwu.edu).
-#
-# You can find some cheatsheets to do other basic stuff like bold-face, italicize, tables, etc.
-
-# %%
